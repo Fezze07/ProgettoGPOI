1e774120fb0b31fc0801088bca6ab92bed323450
Federico_Server <ytfezze@gmail.com>
2026-02-08 11:05:09 +0000

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: __dirname + '.env' });
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rotte
+const pingRoutes = require("./routes/ping");
+
+app.use("/", pingRoutes);
+
+// Backend/index.js
+const PORT = process.env.BACKEND_PORT || 5002;;
+app.listen(PORT, '0.0.0.0', () => console.log(`AFSF Backend attivo su porta ${PORT}`));
---
+++ Frontend/app/page.js
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export default function Home() {
+  const [backendMessage, setBackendMessage] = useState(null);
+
+  useEffect(() => {
+    async function checkBackend() {
+      try {
+        const res = await fetch(`${backendUrl}/ping`);
+        if (!res.ok) {
+        const errorText = await res.text();
+        console.log("Errore Raw dal server:", errorText);
+        setBackendMessage(`Il server ha risposto con errore ${res.status}: ${errorText.substring(0, 100)}...`);
+        return;
+      }
+
+      const data = await res.json();
+        setBackendMessage(`Backend dice: ${JSON.stringify(data)}`);
+      } catch (err) {
+        setBackendMessage(`Errore nella chiamata al backend: ${err.message}`);
+      }
+    }
+    checkBackend();
+  }, []);
+
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Progetto GPO 🚀</h1>
+      <p>Frontend online e funzionante.</p>
+      {backendMessage && <p>{backendMessage}</p>}
+    </main>
+  );
+}
